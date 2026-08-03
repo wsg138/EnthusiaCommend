4229d596e7183bb1dc3ebbb966292fed4a4e4ed6
P2wn <racecarboy77@gmail.com>
2026-08-03 13:35:21 -0400

    Fix stalking movement routing and zone separation

--- src/main/java/org/enthusia/rep/region/LogicalZoneResolver.java
+++ src/main/java/org/enthusia/rep/region/LogicalZoneResolver.java
@@ -4,8 +4,22 @@ package org.enthusia.rep.region;
 public final class LogicalZoneResolver {
     private LogicalZoneResolver() { }
 
-    public static RegionManager.LogicalZone resolve(boolean inMarket, boolean inSpawn,
-                                                    boolean inWarzone, boolean managedWorld) {
+    public static RegionManager.LogicalZone resolveGeneral(boolean inSpawn, boolean inWarzone,
+                                                           boolean managedWorld) {
+        if (!managedWorld) {
+            return RegionManager.LogicalZone.OTHER;
+        }
+        if (inSpawn) {
+            return RegionManager.LogicalZone.SPAWN;
+        }
+        if (inWarzone) {
+            return RegionManager.LogicalZone.WARZONE;
+        }
+        return RegionManager.LogicalZone.WILDERNESS;
+    }
+
+    public static RegionManager.LogicalZone resolveStalking(boolean inMarket, boolean inSpawn,
+                                                            boolean inWarzone, boolean managedWorld) {
         if (!managedWorld) {
             return RegionManager.LogicalZone.OTHER;
         }
